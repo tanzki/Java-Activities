d11fb534baca7bb06be26e620f7bd3ad206b21f8
Tanzki <73052592+tanzki@users.noreply.github.com>
2023-11-20 14:58:16 +0800

    Update MyDate.java

--- Task121/MyDate.java
+++ Task121/MyDate.java
@@ -1,11 +1,15 @@
 package Task121;
-import Task121.DateTask;
+
 public class MyDate {
-    public static void main(String[] args) {
-        DateTask date1 = new DateTask();
-        DateTask date2 = new DateTask();
+
+	public static void main(String[] args) {
+	    DateTask date1 = new DateTask(1,1,1978);
+        DateTask date2 = new DateTask(9,21,1984);
         System.out.println(date1.toString());
         System.out.println(date2.toString());
-       // display leapyears
-    }
+       // display leap years
+        DateTask.leapYears();
+        
+	}
+
 }
